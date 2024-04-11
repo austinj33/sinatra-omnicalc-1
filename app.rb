b284bcfafc6615917d89fe99469c4cebb4674039
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-04-11 02:43:14 +0000

    payments_updated

--- app.rb
+++ app.rb
@@ -2,8 +2,41 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:layout)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_results = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_results = @the_num ** 0.5
+
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_f
+  @principal = params.fetch("user_pv").to_f
+
+  @payment = @principal * (@apr / 12) / (1 - (1 + @apr / 12)**-(@years * 12))
+  
+  erb(:payment_results)
 end
